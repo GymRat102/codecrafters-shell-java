aa027127c3ce07ad27cb761e10a249bd90e340ac
weijian.duan <weijianduan0302@gmail.com>
2026-01-18 20:55:06 +0800

    implement repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
+        for(;;) {
+            System.out.print("$ ");
 
-        System.out.print(command + ": command not found");
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+
+            System.out.println(command + ": command not found");
+        }
     }
 }
